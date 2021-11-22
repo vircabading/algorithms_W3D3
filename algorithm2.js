919678e0e245d390b8d8e1977e9bf0b3570dfd73
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-21 21:36:14 -0800

    add bookindex function

--- algorithm2.js
+++ algorithm2.js
@@ -22,30 +22,30 @@ const two_expected2 = "1, 13-18, 37-38, 70-75";
  *    page ranges.
  */
 
-2.1 store where the range starts (index)
-2.2      if range breaks, then subtract the difference in the indexes
-if the array at the idx+1 = current idx value+1 then store the current index 
+// 2.1 store where the range starts (index)
+// 2.2      if range breaks, then subtract the difference in the indexes
+// if the array at the idx+1 = current idx value+1 then store the current index 
 
-////////// PSEUDO CODE /////////////////////////
-1. Define the function with parameters 1. array of numbers 
-2. output string
-3. in a range boolean
-    current dash value
-3. loop through the array
-4. if not in range then 
-    4.05 if current value is NOT consecutive to the next value, then 
-    4.1      push that index to the output
-    5. if the current value is consecutive to the next value
-    5.1      push the indix to the output
-    5.2      add a dash seperator
-    5.3      say we are in range (true)
-    5.4      store value of of next in range
+// ////////// PSEUDO CODE /////////////////////////
+// 1. Define the function with parameters 1. array of numbers 
+// 2. output string
+// 3. in a range boolean
+//     current dash value
+// 3. loop through the array
+// 4. if not in range then 
+//     4.05 if current value is NOT consecutive to the next value, then 
+//     4.1      push that index to the output
+//     5. if the current value is consecutive to the next value
+//     5.1      push the indix to the output
+//     5.2      add a dash seperator
+//     5.3      say we are in range (true)
+//     5.4      store value of of next in range
 
-6 if in range
-    6.1 if idx +1 is consecutive to dash value
-        6.1 dash value = value of idx+1
-    7. otherwise
-        store value of idx to output
+// 6 if in range
+//     6.1 if idx +1 is consecutive to dash value
+//         6.1 dash value = value of idx+1
+//     7. otherwise
+//         store value of idx to output
 
 function bookIndex(nums) {
     var output = "";
@@ -58,8 +58,24 @@ function bookIndex(nums) {
                 output += nums[i] + ", ";
             }
             if ( nums[i+1] == nums[i] + 1) {        // if consecutive
-                
+                output += nums[i];                   //  Push index to output
+                output += "-"                       //  add a dash seperator to output
+                in_range = true;                    //  note the we are in range
+                current_dash_value = nums[i+1];     //  store the value of the next in range
+            }
+        } 
+        else {                                              // If In Range
+            if ( nums[i+1] == (current_dash_value+1) ) {    //      If idx+1 is consecutive to fdash value
+                current_dash_value = nums[i+1];             //      dash value = value of idx+1
+            }
+            else {
+                output += nums[i] + " ";
+                in_range = false;
             }
         }
     }
+    return output;
 }
+
+console.log("Running Twu Nums1:", bookIndex(two_nums1));
+console.log("Running Twu Nums2:", bookIndex(two_nums2));
